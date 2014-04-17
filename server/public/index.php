aaad7ef4fddd0164310007cb93f3efb76b9fec10
Phillip Whelan <pwhelan@mixxx.org>
2014-04-17 09:05:42 -0700

    Add worker process (invokes timeout, removed from timez-state script).

--- server/public/index.php
+++ server/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 
 use Carbon\Carbon;
@@ -14,6 +14,7 @@ $app->group('/task', function() use ($app) {
 	$app->add(new \JsonApiMiddleware());
 	
 	$app->get('/(:id)', function($id = 0) use ($app) {
+		
 		if ($id) $app->tasks->findOne(['_id-' => new MongoId($id)]);
 		else $task = $app->tasks->findOne(['active' => true]);
 		
@@ -31,7 +32,8 @@ $app->group('/task', function() use ($app) {
 			->format('c');
 		
 		$app->render(200, $task);
-	});
+		
+	})->conditions(['id' => '[a-zA-Z\d]+']);
 	
 	$app->post('/stop/(:id(/:offset))', function($id = 0, $offset = 0) use ($app) {
 		
@@ -81,6 +83,38 @@ $app->group('/task', function() use ($app) {
 		$state = json_decode($app->request->getBody());
 		$state->date = new MongoDate();
 		
+		
+		$shm = shm_attach($app->shared->id);
+		
+		if (shm_has_var($shm, $app->shared->worker_pid))
+		{
+			$worker_pid = shm_get_var($shm, $app->shared->worker_pid);
+			
+			
+			if (shm_get_var($shm, $app->shared->idletimes))
+			{
+				$idletimes = shm_get_var($shm, $app->shared->idletimes);
+				
+				if (isset($idletimes[$app->request->getIp()]))
+				{
+					$last_idle = $idletimes[$app->request->getIp()];
+					if ($state->idle <= $last_idle && $state->idle < (60 * 5))
+					{
+						pcntl_kill($worker_pid, SIGALRM);
+					}
+				}
+				
+				$idletimes[$app->request->getIp()] = $state->idle;
+			}
+			else
+			{
+				$idletimes = [$app->request->getIp() => $state->idle];
+			}
+			
+			shm_put_var($shm, $app->shared->idletimes, $idletimes);
+		}
+		
+		
 		$task = $app->tasks->update(
 			$id == null ?
 				['active' => true] : 
@@ -109,6 +143,34 @@ $app->group('/task', function() use ($app) {
 		);
 		
 		$app->render(201, []);
+	});
+	
+	
+	$app->get('/worker', function() use ($app) {
+		
+		if (php_sapi_name() != 'cli')
+		{
+			die("WRONG SAPI=".php_sapi_name()."\n");
+		}
+		
+		$shm = shm_attach($app->shared->id);
+		shm_put_var($shm, $app->shared->worker_pid, getmypid());
+		
+		pcntl_signal(SIGALRM, function() use ($app) {
+			
+			$app->tasks->update(
+				['active' => true],
+				['$set' => ['active' => false]]
+			);
+			
+		});
+		
+		pcntl_alarm(60 * 5);
+		while (1):
+			$info = [];
+			sleep(5);
+		endwhile;
+			
 	});
 });
 
@@ -143,6 +205,14 @@ $app->container->singleton('tasks', function () {
 	$m = new MongoClient(); // connect
 	$db = $m->timez; // get the database named "foo"
 	return $db->tasks;
+});
+
+$app->container->singleton('shared', function() {
+	return (object)[
+		'id'		=> 0xf00f0000,
+		'worker_pid'	=> 0xf00f0001,
+		'idletimes'	=> 0xf00f0002
+	];
 });
 
 $app->run();
---
+++ server/worker.php
@@ -0,0 +1,26 @@
+<?php
+
+
+require_once __DIR__ .'/vendor/autoload.php';
+
+
+System_Daemon::setOption("appName", "timez-worker");  // Minimum configuration
+System_Daemon::setOption("logLocation", __DIR__.'/log/timez-worker.log');
+System_Daemon::setOption("appPidLocation", __DIR__.'/run/timez-worker/worker.pid');
+
+System_Daemon::setOption("appRunAsUID", getmyuid());
+System_Daemon::setOption("appRunAsGID", getmyuid());
+System_Daemon::setOption("appDieOnIdentityCrisis", false);
+
+System_Daemon::start();
+
+
+$_SERVER['REQUEST_URI'] = '/task/worker';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+$_SERVER['SERVER_NAME'] = 'localhost';
+$_SERVER['SERVER_PORT'] = 80;
+
+include __DIR__.'/public/index.php';
+
+$app->run();
