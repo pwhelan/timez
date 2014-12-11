ec4e78fa7118b12db200fc11a350012907152704
Phillip Whelan <pwhelan@mixxx.org>
2014-12-11 09:44:33 -0800

    Add support for POST variables (url encoded, multipart and JSON).

--- app/Application.php
+++ app/Application.php
@@ -9,48 +9,124 @@ use Silex\ControllerCollection;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
+use h4cc\Multipart\ParserSelector;
+
 
 class Application extends \React\Espresso\Application
 {
 	public function __invoke(Request $request, Response $response)
 	{
 		$content = "";
-		$request->on('data', function($data) use (&$content, $request) {
+		
+		
+		$request->on('data', function($data) use (&$content, &$curlen, $request) {
 			
-			$content .= $data;
-			if (strlen($data) >= $request->getHeaders()['Content-Length'])
+			$headers = $request->getHeaders();
+			
+			if (isset($headers['Transfer-Encoding']) && $headers['Transfer-Encoding'] == 'chunked')
 			{
-				$request->emit('flush');
+				
+			}
+			else
+			{
+				if (!isset($headers['Content-Length']))
+				{
+					$request->emit('flush');
+					return;
+				}
+				
+				$content .= $data;
+				if (strlen($data) >= $headers['Content-Length'])
+				{
+					$request->emit('flush');
+				}
 			}
 		});
 		
-		$request->on('flush', function() use (&$content, $request, $response, $result) {
+		$request->on('flush', function() use (&$content, $request, $response) {
 			
-			// Create Request with the full content.
-			$fullrequest = SymfonyRequest::create(
-				$request->getPath(),
-				$request->getMethod(),
-				$request->getQuery(),
-				/*
-				array_merge(
-					$request->getQuery(),
-					parse($content)
-				)
-				*/
-				[], //$_COOKIES,
-				[], // parse the goddamn input...
-				$_SERVER,
-				$content
-			);
-			
-			$fullrequest->attributes->set('react.espresso.request', $request);
-			$fullrequest->attributes->set('react.espresso.response', $response);
-			
-			$result = $this->handle($fullrequest, HttpKernelInterface::MASTER_REQUEST, true);
+			$sfrequest = $this->buildSfRequest($request, $response, $content);
+			$result = $this->handle($sfrequest, HttpKernelInterface::MASTER_REQUEST, true);
 			
 			$response->writeHead($result->getStatusCode(), $result->headers->all());
 			$response->end($result->getContent());
 		});
-		
 	}
+	
+	protected function buildSfRequest(Request $request, Response $response, &$content)
+	{
+		list ($post, $files) = $this->parseBody($request->getHeaders(), $content);
+		
+		
+		// Create Request with the full content.
+		$sfrequest = SymfonyRequest::create(
+			$request->getPath(),
+			$request->getMethod(),
+			array_merge(
+				$request->getQuery(),
+				$post
+			),
+			[], // $_COOKIES ...
+			[], // $_FILES ...
+			$_SERVER,
+			$content
+		);
+		
+		$sfrequest->attributes->set('react.espresso.request', $request);
+		$sfrequest->attributes->set('react.espresso.response', $response);
+		
+		return $sfrequest;
+	}
+	
+	protected function parseBody($headers, &$content)
+	{
+		$post = [];
+		$files = [];
+		
+		
+		$ContentType = strtolower(
+			strpos($hdr['Content-Type'], ';') === FALSE ?
+				@$hdr['Content-Type'] :
+				substr($hdr['Content-Type'], 0, strpos($hdr['Content-Type'], ';'))
+		);
+		
+		
+		switch($ContentType) 
+		{
+		case 'multipart/form-data':
+			$selector = new ParserSelector();
+			$parser = $selector->getParserForContentType($hdr['Content-Type']);
+			$multipart = $parser->parse($content);
+			
+			
+			foreach ($multipart as $part)
+			{
+				if (isset($part['dispositions']))
+				{
+					$disposition = $part['dispositions'];
+					if (!isset($disposition['filename']))
+					{
+						$post[$disposition['name']] = $part['body'];
+					}
+					else
+					{
+						throw new \Exception('WIP: FILES via MultiPart');
+					}
+				}
+			}
+			break;
+			
+		case 'application/x-www-form-urlencoded':
+			case (!in_array($request->getMethod(), ['GET', 'HEAD'])):
+			parse_str($content, $post);
+			break;
+			
+		case 'application/json':
+			$post = json_decode($content);
+			break;
+		}
+		
+		return [$post, $files];
+	}
+			
 }
